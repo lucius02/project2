5e11a0086ec03fc75806889cea1567c4dadb772f
lucius02 <54938206+lucius02@users.noreply.github.com>
2020-03-06 17:22:18 +0100

    yolo

---
+++ Scripts/Script.sql
@@ -0,0 +1,24 @@
+drop table if exists public.events_event;
+create table public.events_event (
+id serial primary key not null references sch_evenement.evenement(evenement_id),
+Title text not null,
+Naam text NOT null,
+Beschrijving text NOT null,
+Begin_datum text NOT null,
+Eind_datum text NOT null
+);
+
+comment on table public.events_event
+	is 'tabel over de evenement die worden laten gezien op de website';
+comment on column public.events_event.id 
+	is 'primary key en foreign key, het id van het evenement';
+comment on column public.events_event.title 
+	is 'titel van het evenement';
+comment on column public.events_event.naam 
+	is 'naam van organisator van het evenement';
+comment on column public.events_event.beschrijving 
+	is 'beschrijving van het evenement';
+comment on column public.events_event.begin_datum 
+	is 'begin datum van het evenement';
+comment on column public.events_event.eind_datum 
+	is 'eind datum van het evenement';
