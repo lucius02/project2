24ca4fa4b6c872ad99de7133f21f7e76e1bf4a1a
lucius02 <54938206+lucius02@users.noreply.github.com>
2020-03-13 13:40:29 +0100

    goodjob

--- Scripts/back-up_script.sql
+++ Scripts/back-up_script.sql
@@ -3,17 +3,22 @@ drop table if exists sch_evenement.back_up;
 select * into sch_evenement.back_up
 from public.events_event ee 
 ;
--- wilt niet werken --
-update
-	sch_evenement.back_up
-set
-	id = ee.id
-from 
-	sch_evenement.back_up bp 
-	inner join events_event ee
-	on bp.id = ee.id 
-where 
-	bp.begin_datum <= bp.eind_datum
-	and 
-	ee.begin_datum <= ee.begin_datum 
+
+ALTER TRIGGER trigger_backup_update
+   ON sch_evenement.back_up 
+   AFTER UPDATE
+AS BEGIN
+    SET NOCOUNT ON;    
+
+    UPDATE back_up 
+    SET modified = GETDATE()
+       , ModifiedUser = SUSER_NAME()
+       , ModifiedHost = HOST_NAME()
+    FROM back_up bck 
+    INNER JOIN Inserted I ON bck.OrderNo = I.OrderNo and bck.PartNumber = I.PartNumber
+    INNER JOIN Deleted D ON bck.OrderNo = D.OrderNo and bck.PartNumber = D.PartNumber                  
+    WHERE bck.QtyToRepair <> I.QtyToRepair
+    AND D.QtyToRepair <> I.QtyToRepair
+END
 ;
+
