4f8de0947123dbbace343cca948ce9e671531a0d
lucius02 <54938206+lucius02@users.noreply.github.com>
2020-03-20 11:09:43 +0100

    test

--- Scripts/backup_procedure.sql
+++ Scripts/backup_procedure.sql
@@ -1,5 +1,5 @@
 BACKUP database project2
-to disk = 'onedrive:\documenten\project2\backup'
+to disk = 'C:\documenten\project2\backup'
 with stats
 go;
 -- https://www.w3schools.com/sql/sql_backup_db.asp --
