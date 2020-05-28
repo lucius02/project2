7fe869dc069b9c97e28c5490fe0e93edc3298920
lucius02 <54938206+lucius02@users.noreply.github.com>
2020-05-28 09:33:53 +0200

    good

--- Scripts/Script.sql
+++ Scripts/Script.sql
@@ -8,13 +8,13 @@ create schema if not exists main;
 drop table if exists main.vacature cascade;
 create table main.vacature (
 vacature_id serial primary key not null,
-naam varchar,
-job varchar,
-locatie varchar,
-salaris money,
-opleiding varchar,
-taal varchar,
-com_taal varchar
+naam varchar not null,
+job varchar not null,
+locatie varchar not null,
+salaris money not null,
+opleiding varchar not null,
+taal varchar not null,
+com_taal varchar not null
 );
 
 COMMENT ON TABLE main.vacature
@@ -39,3 +39,41 @@ comment on column main.vacature.com_taal
 insert into main.vacature(naam, job, locatie, salaris, opleiding, taal, com_taal)
 values ('test', '25-05-2020', 'IJsselstein', '3000,00', 'HBO', 'Nederlands, Engels', 'python')
 ;
+
+drop table if exists main.zoekpagina;
+create table main.zoekpagina (
+zoekpagina_id serial primary key not null,
+zoek_url varchar not null,
+onderwerp varchar not null
+);
+
+insert into main.zoekpagina(zoek_url, onderwerp)
+values ('https://www.indeed.nl/python', 'python'),
+	   ('https://www.indeed.nl/mysql', 'mysql')
+;
+
+drop table if exists main.zoekresult;
+create table main.zoekresult (
+zoekresult_id serial primary key not null,
+zoekpagina_id int not null references main.zoekpagina(zoekpagina_id),
+pagina_result varchar not null
+);
+
+create or replace function main.buildzoekscript()
+returns void as $$
+begin 
+drop table if exists main.zoekscript;
+create table main.zoekscript as select coalesce(' curl "'||z.zoek_url||'" -o "%pg_data%\temp\'||z.onderwerp||z.zoekpagina_id||'.txt"','')
+from main.zoekpagina z;
+
+copy main.zoekscript to 'C:\live_vacature\_zoekscript.bat';
+end;
+$$ language plpgsql;
+
+
+
+
+
+
+
+
